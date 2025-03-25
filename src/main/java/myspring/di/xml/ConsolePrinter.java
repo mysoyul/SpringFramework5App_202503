d5b8350f5907f5df6d7ebedc14ca5409dd7b5996
mysoyul <vega2k_@naver.com>
2025-03-25 14:46:23 +0900

    HelloSpringTest update

--- src/main/java/myspring/di/xml/ConsolePrinter.java
+++ src/main/java/myspring/di/xml/ConsolePrinter.java
@@ -6,6 +6,7 @@ public class ConsolePrinter implements Printer {
 				" 기본 생성자 호출됨");
 	}
 	public void print(String message) {
+		System.out.println("ConsolePrinter print() 메서드");
 		System.out.println(message);
 	}
 }
--- src/test/java/myspring/di/xml/HelloSpringTest.java
+++ src/test/java/myspring/di/xml/HelloSpringTest.java
@@ -1,11 +1,39 @@
 package myspring.di.xml;
 
+import javax.annotation.Resource;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = "classpath:spring_di.xml")
 public class HelloSpringTest {
-
+	//Hello Type인 스프링빈을 주입받는다. 
+	//hello변수명과 동일한 id를 가진 스프링빈을 주입받는다. 
+	@Autowired
+	Hello hello;
+	
+	//Hello Type인 스프링빈을 주입받는다.
+	//@Qualifier의 값과 동일한 id를 가진 스프링빈을 주입받는다. 
+	@Autowired
+	@Qualifier("helloC")
+	Hello hello1;
+	
+	//@Resource의 name속성의 값과 동일한 id를 가진 스프링빈을 주입받는다. 
+	@Resource(name = "hello")
+	Hello hello2;
+	
+	@Test
+	void helloBean() {
+		System.out.println(hello.sayHello());
+		System.out.println(hello1.sayHello());
+		System.out.println(hello2.sayHello());
+		
+		hello1.print();
+	}
+	
 }
