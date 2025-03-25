25fe691e971541459251eeb4af9ab499289298aa
mysoyul <vega2k_@naver.com>
2025-03-25 16:44:02 +0900

    HelloBeanConfigTest update

--- src/test/java/myspring/di/annot/config/HelloBeanConfigTest.java
+++ src/test/java/myspring/di/annot/config/HelloBeanConfigTest.java
@@ -3,9 +3,18 @@ package myspring.di.annot.config;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = HelloBeanConfig.class)
+@ContextConfiguration(classes = HelloBeanConfig.class,
+loader = AnnotationConfigContextLoader.class)
+/*
+ * Xml 대신 설정역할을 Config 클래스를 작성함
+ * Xml => GenericXmlApplicationContext 가 컨테이너 역할을 담당함
+ * Config 클래스 => AnnotationConfigApplicationContext 가  컨테이너 역할을 담당함
+ * AnnotationConfigContextLoader 로더 클래스의 역할
+ *  : 변경된 AnnotationConfigApplicationContext 컨테이너를 로드한다. 
+ */
 public class HelloBeanConfigTest {
 
 }
