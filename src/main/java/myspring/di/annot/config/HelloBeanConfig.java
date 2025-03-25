6280ee49ce78f95ce0ec663d51c607ee8540b102
mysoyul <vega2k_@naver.com>
2025-03-25 16:39:04 +0900

    HelloBeanConfigTest add

--- src/main/java/myspring/di/annot/config/HelloBeanConfig.java
+++ src/main/java/myspring/di/annot/config/HelloBeanConfig.java
@@ -2,13 +2,17 @@ package myspring.di.annot.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 /*
  * <context:component-scan base-package="myspring.di.annot"/> 
  * ==> @ComponentScan(basePackages={"myspring.di.annot"})
+ * <context:property-placeholder location="classpath:values.properties" />
+ * ==> @PropertySource(value="classpath:values.properties")
  */
 @Configuration
 @ComponentScan(basePackages = {"myspring.di.annot"})
+@PropertySource(value = "classpath:values.properties")
 public class HelloBeanConfig {
 
 }
---
+++ src/test/java/myspring/di/annot/config/HelloBeanConfigTest.java
@@ -0,0 +1,11 @@
+package myspring.di.annot.config;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = HelloBeanConfig.class)
+public class HelloBeanConfigTest {
+
+}
