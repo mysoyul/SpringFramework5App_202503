18e53b990c8a579673bece906a5ef5b85d767556
mysoyul <vega2k_@naver.com>
2025-03-25 15:31:35 +0900

    HelloBeanSpringTest update

--- src/test/java/myspring/di/annot/HelloBeanSpringTest.java
+++ src/test/java/myspring/di/annot/HelloBeanSpringTest.java
@@ -1,11 +1,31 @@
 package myspring.di.annot;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.annotation.Resource;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = "classpath:spring_di.xml")
 public class HelloBeanSpringTest {
-
+	@Autowired
+	HelloBean hello;
+	
+	@Resource(name="stringPrinter")
+	PrinterBean printer;
+	
+	@Test
+	void hello() {
+		assertEquals("Hello 어노테이션", hello.sayHello());
+		hello.print();
+		assertEquals("Hello 어노테이션", printer.toString());
+	}
+	
+	
+	
 }
