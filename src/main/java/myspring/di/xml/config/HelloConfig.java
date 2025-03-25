1537ab0358a7ff6cd66d399b29d39fd4b08b9736
mysoyul <vega2k_@naver.com>
2025-03-25 17:03:32 +0900

    HelloConfig update

--- src/main/java/myspring/di/xml/config/HelloConfig.java
+++ src/main/java/myspring/di/xml/config/HelloConfig.java
@@ -1,10 +1,26 @@
 package myspring.di.xml.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+
+import myspring.di.xml.ConsolePrinter;
+import myspring.di.xml.Printer;
+import myspring.di.xml.StringPrinter;
 
 @Configuration
 @PropertySource(value = "classpath:values.properties")
 public class HelloConfig {
 
+	@Bean
+	public Printer stringPrinter() {
+		return new StringPrinter();
+	}
+	
+	@Bean
+	public Printer consolePrinter() {
+		return new ConsolePrinter();
+	}
+	
+	
 }
