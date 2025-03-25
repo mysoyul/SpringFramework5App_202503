5d8545cc7b3d012451987872d53d3183b8819d5f
mysoyul <vega2k_@naver.com>
2025-03-25 13:36:34 +0900

    assertSame call

--- src/test/java/myspring/di/xml/HelloJunitTest.java
+++ src/test/java/myspring/di/xml/HelloJunitTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
+//Assertions 클래스가 제공하는 모든 static 메서드를 import 한다.
+import static org.junit.jupiter.api.Assertions.*;
+
 public class HelloJunitTest {
 	@Test
 	void hello() {
@@ -13,6 +16,9 @@ public class HelloJunitTest {
 		//2. Container에게  Hello 스프링빈을 요청하기
 		Hello hello = (Hello)context.getBean("hello");
 		Hello helloBean = context.getBean("hello", Hello.class);
-		
+		//3. Hello 스프링빈 의 Singleton Object 여부 확인
+		System.out.println(hello == helloBean);
+		//주소 비교 assertSame() 호출
+		assertSame(hello, helloBean);
 	}
 }
