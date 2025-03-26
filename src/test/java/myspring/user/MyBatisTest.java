281a91881b277458a3a5386d5fd2a811075993d6
mysoyul <vega2k_@naver.com>
2025-03-26 09:29:43 +0900

    MyBatisTest add

--- src/test/java/myspring/user/MyBatisTest.java
+++ src/test/java/myspring/user/MyBatisTest.java
@@ -1,5 +1,11 @@
 package myspring.user;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = "classpath:spring_beans.xml")
 public class MyBatisTest {
 
 }
