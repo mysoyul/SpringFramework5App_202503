9b9c4e5ebe7da82ca61ce0397856f464b0b185a2
mysoyul <vega2k_@naver.com>
2025-03-26 13:48:27 +0900

    MyBatisStudentTest update

--- src/test/java/myspring/student/MyBatisStudentTest.java
+++ src/test/java/myspring/student/MyBatisStudentTest.java
@@ -1,11 +1,25 @@
 package myspring.student;
 
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import myspring.student.dao.mapper.StudentMapper;
+import myspring.student.vo.StudentVO;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = "classpath:spring_beans.xml")
 public class MyBatisStudentTest {
-
+	@Autowired
+	StudentMapper mapper;
+	
+	@Test
+	void student1() {
+		List<StudentVO> stuList = 
+				mapper.selectStudentDept();
+	}
 }
